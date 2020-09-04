fb6387093fb0fb3664216a727317dddc584f52bf
Hritik Kumar <hrk10321@gmail.com>
2020-09-04 11:02:55 +0530

    Added accountConnectionMarkup()

--- src/SettingsForm.js
+++ src/SettingsForm.js
@@ -6,6 +6,12 @@ import {
 } from '@shopify/polaris'
 
 class SettingsForm extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            connected :false
+        }
+    }
     render() {
         return(
             <Layout>
@@ -13,21 +19,38 @@ class SettingsForm extends Component{
                 title="Connected User"
                 description="Shopify and your customers will use this information to contact you."
             >
-            <AccountConnection
-            title="Dropshhip"
-            action={{
-                content: "Connect"
-            }}
-            details="No account Connected"
-            termsOfService={<p>
-                 By clicking <strong>Connect</strong>, you agree to accept Sample App’s{' '}
-                <Link url="#">terms and conditions</Link>. You’ll pay a
-                commission rate of 15% on sales made through Dropshhip.
-            </p>}
-            /> 
+            {this.accountConnectionMarkup()}
             </Layout.AnnotatedSection>
             </Layout>
         );
+    }
+
+    toggleConnection(){
+        this.setState(({connected}) => ({connected : !connected}));
+    }
+
+    accountConnectionMarkup(){
+        return this.state.connected
+        ?(
+
+            <AccountConnection 
+                avatarUrl=""
+                accountName="XYZ"
+                details="xyz.com"
+                action={{content: 'Disconnect', onAction: this.toggleConnection.bind(this)}}
+                connected={this.state.connected}/>
+        ):(
+            <AccountConnection
+                title="Dropshipp"
+                action={{content: 'Connect', onAction: this.toggleConnection.bind(this)}}
+                connected={this.state.connected}
+                details='No account Connected'
+                termsOfService={<p>
+                    By clicking <strong>Connect</strong>, you agree to accept Sample App’s{' '}
+                   <Link url="#">terms and conditions</Link>. You’ll pay a
+                   commission rate of 15% on sales made through Dropshhip.
+               </p>}/>
+        )
     }
 }
 
