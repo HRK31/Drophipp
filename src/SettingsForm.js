a5a3832d080e4329267d0a6cfc61113bff0f8e8e
Hritik Kumar <hrk10321@gmail.com>
2020-09-04 12:01:19 +0530

    Added other sections

--- src/SettingsForm.js
+++ src/SettingsForm.js
@@ -2,31 +2,97 @@ import React, {Component} from 'react';
 import {
     Layout,
     AccountConnection,
-    Link
+    Link,
+    Card,
+    Checkbox,
+    TextField,
+    RadioButton,
+    Stack
 } from '@shopify/polaris'
 
 class SettingsForm extends Component{
     constructor(props){
         super(props);
         this.state = {
-            connected :false
+            connected :false,
+            autoPublish: false,
         }
     }
     render() {
         return(
-            <Layout>
-            <Layout.AnnotatedSection
-                title="Connected User"
-                description="Shopify and your customers will use this information to contact you."
-            >
-            {this.accountConnectionMarkup()}
-            </Layout.AnnotatedSection>
-            </Layout>
+            <form>
+                <Layout>
+                <Layout.AnnotatedSection
+                    title="Connected User"
+                    description="Shopify and your customers will use this information to contact you."
+                >
+                {this.accountConnectionMarkup()}
+                </Layout.AnnotatedSection>
+
+                <Layout.AnnotatedSection 
+                    title="Selling and shipping"
+                    description="Manage products, pricing, shipping and customer ntifications."
+                >
+                    <Card sectioned title="Products" >
+                        <Checkbox
+                            label="Automatically publish new products"
+                            helpText="New products add in DropShipp will immediately be published to all of your Shopify sales channels."
+                            checked={this.state.autoPublish}
+                            onChange={this.handleInputChange('autoPublish')}
+                            />
+                    </Card>
+                    <Card sectioned title="Pricing rules"></Card>
+                    <Card sectioned title="Shipping">
+                    <Checkbox
+                            label="Automatically publish new products"
+                            helpText="New products add in DropShipp will immediately be published to all of your Shopify sales channels."
+                            checked={this.state.autoShip}
+                            onChange={this.handleInputChange('autoShip')}
+                            />
+                            <TextField 
+                            label="Custom Shipment tracking URL" 
+                            helpText="Overrides the normal shipment tracking link enabled to your Customer.Learn more about 
+                            custom tracking URLs."
+                            />
+                    </Card>
+                </Layout.AnnotatedSection>
+
+                <Layout.AnnotatedSection 
+                    title="Reporting"
+                    description="Manage how you track success with Dropshipp"
+                >
+                    <Card sectioned title="Receive reports via email:">
+                        <Stack vertical>
+                        <RadioButton
+                            label="Never"
+                            id="never"
+                        />
+                        <RadioButton
+                            label="Daily"
+                            id="daily"
+                        />
+                        <RadioButton
+                            label="Weekly"
+                            id="weekly"
+                        />
+                        <RadioButton
+                            label="Monthly"
+                            id="monthly"
+                        />
+                        </Stack>
+                    </Card>
+                </Layout.AnnotatedSection>
+                </Layout>
+            </form>
         );
     }
 
     toggleConnection(){
         this.setState(({connected}) => ({connected : !connected}));
+    }
+
+    handleInputChange(field){
+        return (value) => this.setState({[field]: value})
     }
 
     accountConnectionMarkup(){
